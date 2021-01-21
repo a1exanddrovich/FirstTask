d2882834729fe5d240731a72307133a1a994ece8
a1exanddrovich <l.bliznichenko@mail.ru>
2021-01-21 16:28:13 +0300

    Tests have been added

---
+++ src/main/java/com/epam/task/first/logic/entities/Array.java
@@ -0,0 +1,18 @@
+package com.epam.task.first.logic.entities;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Array {
+
+    private final List<Integer> elements;
+
+    public Array(List<Integer> elements) {
+        this.elements = elements;
+    }
+
+    public List<Integer> getElements() {
+        return elements;
+    }
+
+}
---
+++ src/main/java/com/epam/task/first/logic/logic/ArrayLogic.java
@@ -0,0 +1,11 @@
+package com.epam.task.first.logic.logic;
+
+import com.epam.task.first.logic.entities.Array;
+
+public class ArrayLogic {
+
+        public int findMax(Array array) {
+            return 3;
+        }
+
+}
